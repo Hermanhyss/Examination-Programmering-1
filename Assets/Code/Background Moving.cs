facdf2fc7633c79dd6df0416695893a4adf5e217
Oscar Hermansson <oscar.hermansson@learnet.se>
2023-12-08 14:59:39 +0100

    roation, med movement all ways and

--- Assets/Code/Background Moving.cs
+++ Assets/Code/Background Moving.cs
@@ -8,7 +8,7 @@ public class BackgroundMoving : MonoBehaviour
     [SerializeField]
     private Renderer bgRenderer;
 
-    // Update is called once per frame
+    
     void Update()
     {
         bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
